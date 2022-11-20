aeb8f23cd2739acea1e8fa4c98f1ab1ee0cbc654
alemoles <alemolerio@gmail.com>
2022-11-19 23:03:07 -0300

    Miscellaneous app package & app activity testing

--- src/test/java/com/devstromo/AppiumBasicTest.java
+++ src/test/java/com/devstromo/AppiumBasicTest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 
@@ -118,6 +119,41 @@ public class AppiumBasicTest extends BaseTest {
         // tagName[@attribute='value']
         driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']"))
             .click();
+        driver.findElement(By.id("android:id/checkbox"))
+            .click();
+        DeviceRotation landScape = new DeviceRotation(0, 0, 90);
+
+        driver.rotate(landScape);
+
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]"))
+            .click();
+        String alertTitle = driver.findElement(By.id("android:id/alertTitle"))
+            .getText();
+        assertEquals(alertTitle, "WiFi settings");
+        // copy paste
+        // copy to clipboard- paste it clipboard
+        driver.setClipboardText("AleWifi");
+        String wifiName = driver.getClipboardText();
+        assertEquals("AleWifi", wifiName);
+        driver.findElement(By.id("android:id/edit"))
+            .sendKeys(wifiName);
+
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+
+        driver.findElements(AppiumBy.className("android.widget.Button"))
+            .get(1)
+            .click();
+
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        // press home
+        driver.pressKey(new KeyEvent(AndroidKey.HOME));
+    }
+
+    @Test
+    public void MiscellaneousAppPackageAndActivityTest() throws InterruptedException {
+        // App Package & App Activity
+        Activity activity = new Activity("io.appium.android.apis", "io.appium.android.apis.preference.PreferenceDependencies");
+        driver.startActivity(activity);
         driver.findElement(By.id("android:id/checkbox"))
             .click();
         DeviceRotation landScape = new DeviceRotation(0, 0, 90);
