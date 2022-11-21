a7f375729ef29ca63844e92db293e5af336535ce
alemoles <alemolerio@gmail.com>
2022-11-21 19:55:07 -0300

    Test Case in check toast validation in Ecommerce app

--- src/test/java/com/devstromo/ecommerce/EcommerceTest.java
+++ src/test/java/com/devstromo/ecommerce/EcommerceTest.java
@@ -1,15 +1,15 @@
 package com.devstromo.ecommerce;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.nativekey.AndroidKey;
-import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class EcommerceTest extends BaseTest {
 
     @Test(description = "Test Case in Filling the form details for shopping")
-    public void fillFormTCTest() throws InterruptedException {
+    public void fillFormTCTest() {
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
             .sendKeys("Ale M");
         driver.hideKeyboard();
@@ -22,5 +22,22 @@ public class EcommerceTest extends BaseTest {
             .click();
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
             .click();
+    }
+
+    @Test(description = "Verifying toast messages for error validations")
+    public void verifyToastMessagesTCTest() throws InterruptedException {
+
+        driver.findElement(AppiumBy.xpath("//android.widget.RadioButton[@text='Female']"))
+            .click();
+        driver.findElement(AppiumBy.id("android:id/text1"))
+            .click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Argentina']"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
+            .click();
+        String toastErrorMessage = driver.findElement(AppiumBy.xpath("(//android.widget.Toast)[1]"))
+            .getAttribute("name");
+        assertEquals("Please enter your name", toastErrorMessage);
     }
 }
