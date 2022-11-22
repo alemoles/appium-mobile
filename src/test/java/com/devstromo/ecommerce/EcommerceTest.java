4b872b24eab69e6fde02c738c3ea67d8d306cdbc
alemoles <alemolerio@gmail.com>
2022-11-22 20:29:02 -0300

    Test Case-Dynamically selecting Product by scanning list based on text in Ecommerce app

--- src/test/java/com/devstromo/ecommerce/EcommerceTest.java
+++ src/test/java/com/devstromo/ecommerce/EcommerceTest.java
@@ -2,6 +2,10 @@ package com.devstromo.ecommerce;
 
 import static org.testng.AssertJUnit.assertEquals;
 
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -75,4 +79,44 @@ public class EcommerceTest extends BaseTest {
             .click();
     }
 
+    @Test(description = "Testcase-Dynamically selecting Product by scanning list based on text")
+    public void selectProductDynamicallyTCTest() {
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
+            .sendKeys("Ale M");
+        driver.hideKeyboard();
+        driver.findElement(AppiumBy.xpath("//android.widget.RadioButton[@text='Female']"))
+            .click();
+        driver.findElement(AppiumBy.id("android:id/text1"))
+            .click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Argentina']"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
+            .click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"));"));
+        int productCount = driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productName"))
+            .size();
+        for (int i = 0; i < productCount; i++) {
+            String productName = driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productName"))
+                .get(i)
+                .getText();
+            if (productName.equals("Jordan 6 Rings")) {
+                driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productAddCart"))
+                    .get(i)
+                    .click();
+            }
+        }
+        String cartCount = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/counterText"))
+            .getText();
+        assertEquals("1", cartCount);
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart"))
+            .click();
+
+        // avoid conflicting id com.androidsample.generalstore:id/productName
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+        String lastPageProduct = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/productName"))
+            .getText();
+        assertEquals("Jordan 6 Rings", lastPageProduct);
+    }
 }
