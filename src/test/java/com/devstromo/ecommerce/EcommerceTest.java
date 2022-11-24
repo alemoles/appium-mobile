a7c047b93ccf8e85fbb5b76e9b08bdf294c39225
alemoles <alemolerio@gmail.com>
2022-11-24 20:50:52 -0300

    Handling Web View in Ecommerce app

--- src/test/java/com/devstromo/ecommerce/EcommerceTest.java
+++ src/test/java/com/devstromo/ecommerce/EcommerceTest.java
@@ -1,17 +1,23 @@
 package com.devstromo.ecommerce;
 
+import static io.appium.java_client.android.nativekey.AndroidKey.BACK;
+import static org.openqa.selenium.Keys.ENTER;
 import static org.testng.AssertJUnit.assertEquals;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class EcommerceTest extends BaseTest {
 
@@ -195,7 +201,125 @@ public class EcommerceTest extends BaseTest {
         longPressAction(termsButton);
         driver.findElement(AppiumBy.id("android:id/button1"))
             .click();
-        driver.findElement(AppiumBy.className("android.widget.CheckBox")).click();
-        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnProceed")).click();
+        driver.findElement(AppiumBy.className("android.widget.CheckBox"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnProceed"))
+            .click();
+    }
+
+    @Test(description = "TestCase - Handling Web View")
+    public void handleWebViewTCTest() throws InterruptedException {
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
+            .sendKeys("Ale M");
+        driver.hideKeyboard();
+        driver.findElement(AppiumBy.xpath("//android.widget.RadioButton[@text='Female']"))
+            .click();
+        driver.findElement(AppiumBy.id("android:id/text1"))
+            .click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Argentina']"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
+            .click();
+        driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='ADD TO CART']"))
+            .get(0)
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart"))
+            .click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+        List<WebElement> productPrices = driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productPrice"));
+        double sum = 0;
+        for (WebElement productPrice : productPrices) {
+            String amountString = productPrice.getText();
+            double price = getFormattedAmount(amountString);
+            sum += price;
+        }
+        assertEquals(160.97, sum);
+        String displaySum = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/totalAmountLbl"))
+            .getText();
+        double displayFormattedSum = getFormattedAmount(displaySum);
+        assertEquals(displayFormattedSum, sum);
+        WebElement termsButton = driver.findElement(By.id("com.androidsample.generalstore:id/termsButton"));
+        longPressAction(termsButton);
+        driver.findElement(AppiumBy.id("android:id/button1"))
+            .click();
+        driver.findElement(AppiumBy.className("android.widget.CheckBox"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnProceed"))
+            .click();
+
+        Thread.sleep(10000);
+        Set<String> contexts = driver.getContextHandles();
+        for (String context : contexts) {
+            System.out.println(context);
+        }
+        // continue automation in web view
+        driver.context("WEBVIEW_com.androidsample.generalstore");
+        driver.findElement(AppiumBy.name("q"))
+            .sendKeys("raul shetty academy");
+        driver.findElement(AppiumBy.name("q"))
+            .sendKeys(ENTER);
+        Thread.sleep(5000);
+        driver.pressKey(new KeyEvent(BACK));
+        // SWITCH TO NATIVE
+        driver.context("NATIVE_APP");
+    }
+
+    @Test(description = "TestCase - Automate Hybrid App with context Switch using Chrome Driver")
+    public void hybridAppAndChromeDriverTCTest() throws InterruptedException {
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"))
+            .sendKeys("Ale M");
+        driver.hideKeyboard();
+        driver.findElement(AppiumBy.xpath("//android.widget.RadioButton[@text='Female']"))
+            .click();
+        driver.findElement(AppiumBy.id("android:id/text1"))
+            .click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Argentina']"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"))
+            .click();
+        driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='ADD TO CART']"))
+            .get(0)
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart"))
+            .click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+        List<WebElement> productPrices = driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productPrice"));
+        double sum = 0;
+        for (WebElement productPrice : productPrices) {
+            String amountString = productPrice.getText();
+            double price = getFormattedAmount(amountString);
+            sum += price;
+        }
+        assertEquals(160.97, sum);
+        String displaySum = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/totalAmountLbl"))
+            .getText();
+        double displayFormattedSum = getFormattedAmount(displaySum);
+        assertEquals(displayFormattedSum, sum);
+        WebElement termsButton = driver.findElement(By.id("com.androidsample.generalstore:id/termsButton"));
+        longPressAction(termsButton);
+        driver.findElement(AppiumBy.id("android:id/button1"))
+            .click();
+        driver.findElement(AppiumBy.className("android.widget.CheckBox"))
+            .click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnProceed"))
+            .click();
+
+        Thread.sleep(6000);
+        Set<String> contexts = driver.getContextHandles();
+        for (String context : contexts) {
+            System.out.println(context);
+        }
+        // continue automation in web view
+        driver.context("WEBVIEW_com.androidsample.generalstore");
+        driver.findElement(AppiumBy.name("q"))
+            .sendKeys(ENTER);
+        Thread.sleep(5000);
+        driver.pressKey(new KeyEvent(BACK));
+        // SWITCH TO NATIVE
+        driver.context("NATIVE_APP");
     }
 }
