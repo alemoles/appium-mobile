dc6bf559b9134c9798515470146565644452b943
alemoles <alemolerio@gmail.com>
2022-11-25 21:23:22 -0300

    How to Scroll and put assertions on Browser Tests with Appium in Ecommerce app

--- src/test/java/com/devstromo/ecommerce/MobileBrowserTest.java
+++ src/test/java/com/devstromo/ecommerce/MobileBrowserTest.java
@@ -1,7 +1,10 @@
 package com.devstromo.ecommerce;
 
 import static org.openqa.selenium.Keys.ENTER;
+import static org.testng.AssertJUnit.assertEquals;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -16,5 +19,18 @@ public class MobileBrowserTest extends BrowserBaseTest {
             .sendKeys("rahul shetty academy");
         driver.findElement(AppiumBy.name("q"))
             .sendKeys(ENTER);
+    }
+
+    @Test
+    public void browserWebsiteTest() {
+        driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.xpath("//span[@class='navbar-toggler-icon']"))
+            .click();
+        driver.findElement(By.cssSelector("a[routerlink*='products']")).click();
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,1000)", "");
+        String text = driver.findElement(By.cssSelector("a[href*='products/3']"))
+            .getText();
+        assertEquals(text, "Devops");
     }
 }
